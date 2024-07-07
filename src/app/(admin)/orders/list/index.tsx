7bed0c0e657cea972b5e9a346a1094cb40a19048
yommexg <hydrokidb@gmail.com>
2024-07-07 21:22:55 +0100

    update

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -2,7 +2,7 @@ import { Text, FlatList } from "react-native";
 import orders from "@/assets/data/orders";
 import OrderListItem from "@/src/components/OrderListItem";
 
-export default function OorderScreen() {
+export default function OrderScreen() {
   return (
     <FlatList
       data={orders}
